5c9d722721117152c78a3237ea4aa0f7ce55d4bd
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-05-18 23:10:02 +0900

    取得した画像情報を変数に代入
    検証ツールで確認した際に、e.target.files[0]として発火したEventハッシュに格納されていることが確認できた。
    取得した画像情報を変数に代入。

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list');
+
   document.getElementById('message_image').addEventListener('change', function(e){
-    console.log(e);
+    const file = e.target.files[0];
   });
 });
